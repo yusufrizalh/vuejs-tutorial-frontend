94a671d83695fa098ac3db8f577ec845ad280afc
Yusuf Rizal <yusufrizalh@gmail.com>
2020-04-15 14:09:15 +0700

    Vue.js pada sisi front-end

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <a href="#" class="navbar-brand">bezKoder</a>
+      <a href="#" class="navbar-brand">@yusufrizalh</a>
       <div class="navbar-nav mr-auto">
         <li class="nav-item">
           <a href="/tutorials" class="nav-link">Tutorials</a>
